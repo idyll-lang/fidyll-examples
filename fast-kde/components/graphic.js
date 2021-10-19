533e1860b8445bfc8d33e40f31ae059638d763e2
Matthew Conlen <mc@mathisonian.com>
2021-10-19 13:07:27 -0700

    add data video

--- fast-kde/components/graphic.js
+++ fast-kde/components/graphic.js
@@ -2,9 +2,31 @@ const React = require('react');
 
 class Graphic extends React.Component {
   render() {
-    const { idyll, updateProps, hasError, ...props } = this.props;
+    const { idyll, updateProps, hasError, children, error, title, subtitle, displayParameters, ...props } = this.props;
+
+    console.log('graphic props', props)
     return (
-      <div className="idyll-graphic" {...props} />
+      <div className="idyll-graphic">
+        {(title || subtitle) ?
+          <div>
+            {title ? <div><b>{title}</b></div> : null}
+            {subtitle ? <div>{subtitle}</div> : null}
+          </div>
+        : null}
+        <div>
+          {children}
+        </div>
+        {
+          displayParameters ? <details>
+            <summary>Parameters</summary>
+            {Object.keys(props).map((k) => {
+              return <div key={k}>
+                <div>{k}: {'' + props[k]}</div>
+              </div>
+            })}
+          </details> : null
+        }
+      </div>
     );
   }
 }
